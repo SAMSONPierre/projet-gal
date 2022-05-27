148f2b53e4d164d2f27fa03eda7b01d7c19743a0
ZHANG <fr.zhang.changrui@gmail.com>
2022-05-27 22:33:57 +0200

    detaille transition

--- main.ml
+++ main.ml
@@ -1,5 +1,6 @@
 open Ast 
 let lexbuf = Lexing.from_channel stdin 
+let mots=Array.get Sys.argv 1
 let ast = Parser.input Lexer.main lexbuf
 let _ = Printf.printf "Parse:\n%s\n" (as_string ast)
 
@@ -53,6 +54,78 @@ let rec check_transition_for_states states transitions =
     — la pile est vide sans que l’entrée soit épuisée.
   Deuxième partie : done
 *)
+open Stack;;
+let stringToList str = List.init (String.length str) (String.get str);;
+let excecute ast mot=
+  let mot =create() in
+  let etat = ref ' ' in
+  let pile = create() in
+  let currentCarac= ref ' ' in
+  let currentStack= ref ' ' in
+
+  let rec pushAll lis pi=
+    match lis with 
+    |x::y->push x pi;pushAll y pi 
+    |[]->()
+  in
+  let endTest()=
+    if(!currentStack=' ' && !currentCarac=' ')then
+      print_string "\nEND\n"
+    else
+      print_string "\nEND\n"
+  in
+  let getPileContent pile =
+    let p=Stack.copy pile in
+    let res=ref "" in
+    while not(is_empty p) do
+      res:=(Char.escaped (pop p))^ !res
+    done; 
+    res
+  in
+  let rec deplace newEtat motChange ajoutPile transition=
+    print_string ("\n("^(Char.escaped !etat)^","^ !(getPileContent mot)^(Char.escaped !currentCarac)^","^ !(getPileContent pile)^(Char.escaped !currentStack)^") -> ");
+    etat:=newEtat;
+    pushAll ajoutPile pile;
+    if(motChange)then
+      if(is_empty mot)then
+        currentCarac:=' '
+      else
+        currentCarac:=pop mot;
+    if(is_empty pile)then
+      currentStack:=' '
+    else
+      currentStack:=pop pile;
+    print_string ("("^(Char.escaped !etat)^","^ !(getPileContent mot)^(Char.escaped !currentCarac)^","^ !(getPileContent pile)^(Char.escaped !currentStack)^")\n");
+    if !currentStack = ' ' || !currentCarac= ' ' then
+        endTest()
+    else 
+      goTrans transition transition
+  and
+  goTrans trans transition=
+    match trans with
+      | [] -> print_string "\nEND\n"
+      | a::b ->
+        match a with
+        | (e,None,p1,eArr,p2 ) ->
+                                if e = !etat && !currentStack=p1 then 
+                                  deplace eArr false p2 transition
+                                else goTrans b transition
+        | (e,Some(v2),p1,eArr,p2 ) ->
+                                  if e = !etat && v2 = !currentCarac && !currentStack = p1 then 
+                                  deplace eArr true p2 transition
+                                    else
+                                    goTrans b transition
+  in
+  match ast with
+  |((inputsymbols, stacksymbols, states, initial_state, initial_stack), transitions ) ->
+    etat:= initial_state;
+    push (initial_stack) pile;
+    pushAll (stringToList mots) mot;
+    currentCarac:= (pop mot);
+    currentStack:=(pop pile);
+    goTrans transitions transitions
+;;
+
 
 let est_valide ast = 
   match ast with
@@ -64,3 +137,4 @@ let est_valide ast =
 ;;
 
 let _ = est_valide ast
+let _=excecute ast mots
